e46df496b2db53ee579391537e07120d8cfe4e7e
Akashh Krishh <54142695+akashhkrishh@users.noreply.github.com>
2023-08-16 11:09:21 +0530

    CeilingSearch.java

---
+++ BinarySearch/CeilingSearch.java
@@ -0,0 +1,22 @@
+public class CeilingSearch {
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,5,7,8,10};
+        int target = 3;
+        System.out.print(findCeiling(arr,target));
+    }
+    private static int findCeiling(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length-1;
+        while (start <= end){
+            int mid = start+(end - start)/2;
+            if (arr[mid]==target)
+                return arr[mid];
+            if(arr[mid]>target)
+                end = mid-1;
+            else if (arr[mid]<target) {
+                start = mid+1;
+            }
+        }
+        return arr[start];
+    }
+}
