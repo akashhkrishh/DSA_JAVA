0a2c6626c6d99c6fbc00cc84b0c75bcfd600dd6d
Akashh Krishh <54142695+akashhkrishh@users.noreply.github.com>
2023-08-29 17:23:01 +0530

    BubbleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -3,13 +3,17 @@ public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = {2,42,5,225,532,2};
         for (int i = 0; i < arr.length; i++) {
+            boolean swap = false;
             for (int j = 1; j < (arr.length-i) ; j++) {
                if (arr[j-1] > arr[j]){
                    int t = arr[j];
                    arr[j]=arr[j-1];
                    arr[j-1] =t ;
+                   swap = true;
                }
             }
+            if(!swap)
+                break;
         }
         System.out.println(Arrays.toString(arr));
     }
