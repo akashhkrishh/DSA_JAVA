5e2a2d6d71bf86ed4487774ab8cb2ad2fdc202b6
Akashh Krishh <54142695+akashhkrishh@users.noreply.github.com>
2023-08-31 11:01:13 +0530

    CyclicSort.java

--- Sorting/CyclicSort.java
+++ Sorting/CyclicSort.java
@@ -2,12 +2,15 @@ public class Main
 {
 	public static void main(String[] args) {
 	    int[] arr = {3,1,2,5,4};
-	    for (int i=0;i<arr.length ;i++ ){
-	       int ans = arr[i]-1;
-	       if(arr[ans] != arr[i]){
+	    int i=0;
+	    while (i < arr.length ){
+	       int ans = arr[i];
+	       if(arr[i] < arr.length && arr[ans] != arr[i]){
 	            int t = arr[i];
 	            arr[i] = arr[ans] ;
 	            arr[ans] = t;
+	       }else{
+	           i++;
 	       }
 	    } 
 	   System.out.print(Arrays.toString(arr));    
